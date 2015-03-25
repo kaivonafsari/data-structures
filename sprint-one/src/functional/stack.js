b470a48817c4883dce734ed9669f2cb22fa75dfc
kaivonafsari <kaivon.afsari@gmail.com>
2015-03-25 14:41:12 -0700

    Fix Stack of Functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function(){
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if (size > 0){
+      var result = storage[0];
+      size--;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+  return size;
   };
 
   return someInstance;
