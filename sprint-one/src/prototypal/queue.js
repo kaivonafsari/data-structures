8819091ed61f4dae953995549b077d826d72c75e
kaivonafsari <kaivon.afsari@gmail.com>
2015-03-26 09:08:34 -0700

    Fix Que of Prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
-var Queue = function() {
+var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var storage = Object.create(queueMethods);
+storage.qSize = 0;
+return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function(){
+  return this.qSize;
+};
 
+queueMethods.enqueue = function(value){
+  this[this.qSize] = value;
+  this.qSize++;
+};
+
+queueMethods.dequeue = function(){
+  if (this.qSize > 0){
+    this.qSize--;
+    var result = this[0]
+
+    for (var i =0; i < this.qSize; i++) {
+      this[i] = this[i+1];
+    }
+    delete this[this.qSize];
+    return result;
+  }
+};
