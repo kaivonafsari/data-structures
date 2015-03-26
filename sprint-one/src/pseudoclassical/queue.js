f0ab8ff830ba651d31b595466057d96925bb85cc
kaivonafsari <kaivon.afsari@gmail.com>
2015-03-26 09:58:29 -0700

    Finish Que Psuedo

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
-var Queue = function() {
+var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+this.qSize = 0;
+
 };
 
+
+Queue.prototype.size = function(){
+  return this.qSize;
+};
+
+Queue.prototype.enqueue = function(value){
+  this[this.qSize] = value;
+  this.qSize++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.qSize > 0){
+    this.qSize--;
+    var result = this[0]
+
+    for (var i =0; i < this.qSize; i++) {
+      this[i] = this[i+1];
+    }
+    delete this[this.qSize];
+    return result;
+  }
+};
 
