9d8c08e68f2187dbb232b51cfcff2289775319d5
kaivonafsari <kaivon.afsari@gmail.com>
2015-03-27 20:41:23 -0700

    Finish Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  if(this._storage.indexOf(item) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[this._storage.indexOf(item)]
+  for (var i = 0; i < this._storage.indexOf(item); i++) {
+    this._storage[i] = this._storage[i + 1];
+  }
+  delete this._storage[this._storage.length];
 };
 
 /*
