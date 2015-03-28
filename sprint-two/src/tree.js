8da3fcb5a15caf3f2a9d2d75071d125208242720
kaivonafsari <kaivon.afsari@gmail.com>
2015-03-27 20:10:03 -0700

    Fix Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,17 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+
+  _.extend = function(obj) {
+    for (var i =1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        obj[key] = arguments[i][key]
+      }
+    }
+    return obj;
+  }
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,14 +25,36 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target){
+  //node = node || this;
 
+  var answer = false;
+
+  var finder = function(node, target){
+
+    if (node.value === target) {
+      answer = true;
+    }
+    else{
+      for(var i = 0; i < node.children.length; i++){
+      finder(node.children[i], target);
+      }
+    }
+    console.log(answer)
+  };
+  //node.contains(target,node);
+  finder(this,target)
+  return answer;
 };
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
